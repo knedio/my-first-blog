e12d284ff9262ce046ffc6118d8d9a73fcfe9aab
naio.edio <karlnaio.edio@fligno.com>
2019-09-21 15:55:01 +0800

    Added

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
  	path('post/<int:pk>/', views.post_detail, name='post_detail'),
  	path('post/new/', views.post_new, name='post_new'),
  	path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+ 	path('post/<int:pk>/delete/', views.post_delete, name='post_delete'),
 ]
